03054f84355781a9abb317c75431c77c63bbe207
Younes Strittmatter <younes_strittmatter@brown.edu>
2023-06-24 23:57:24 -0400

    feat: update post gen for basic option

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -12,6 +12,16 @@ def clean_up():
     shutil.rmtree(to_remove)
 
 
+def basic_or_advanced():
+    question_1 = [inquirer.List('advanced',
+                                message="Do you want to use advanced features?",
+                                choices=["yes", "no"],
+                                )
+                  ]
+    answer = inquirer.prompt(question_1)
+    return answer['advanced'] == 'yes'
+
+
 def create_autora_hub_requirements(source_branch, requirements_file):
     response = requests.get(f'https://raw.githubusercontent.com/AutoResearch/autora/{source_branch}/pyproject.toml')
     doc = parse(response.text)
@@ -40,7 +50,6 @@ def create_autora_hub_requirements(source_branch, requirements_file):
             additional_deps += inquirer.prompt(questions)[f'{type}']
 
     # Install packages using pip and the requirements.txt file
-    print(additional_deps)
     with open(requirements_file, 'a') as f:
         for a in additional_deps:
             f.write(f'\n{a}')
@@ -48,9 +57,27 @@ def create_autora_hub_requirements(source_branch, requirements_file):
     return 'autora[experiment-runner-firebase-prolific]' in additional_deps
 
 
+def setup_basic(requirements_file):
+    with open(requirements_file, 'a') as f:
+        f.write('autora')
+    shutil.move(f'example_mains/basic.js', 'testing_zone/src/design/main.js')
+    shutil.move(f'example_workflows/basic.py', 'researcher_hub/autora_workflow.py')
+    shutil.move(f'readmes/README_AUTORA.md', 'researcher_hub/README.md')
+    shutil.move(f'readmes/README_FIREBASE_basic.md', 'testing_zone/README.md')
+
+    # Remove tmps
+    to_remove = os.path.join(os.getcwd(), 'example_workflows')
+    shutil.rmtree(to_remove)
+    to_remove = os.path.join(os.getcwd(), 'example_mains')
+    shutil.rmtree(to_remove)
+    to_remove = os.path.join(os.getcwd(), 'readmes')
+    shutil.rmtree(to_remove)
+
+
+
 def create_autora_example_project():
     question_1 = [inquirer.List('firebase',
-                                message="Do you want to set up a firebase experiment? (ATTENTION: Node to be installed for this feature)",
+                                message="Do you want to set up a firebase experiment? (ATTENTION: Node is required for this feature)",
                                 choices=["yes", "no"],
                                 )
                   ]
@@ -109,8 +136,11 @@ def main():
     source_branch = 'main'
     project_directory = os.path.join(os.path.realpath(os.path.curdir), 'researcher_hub')
     requirements_file = os.path.join(project_directory, 'requirements.txt')
-    if create_autora_hub_requirements(source_branch, requirements_file):
-        create_autora_example_project()
+    if basic_or_advanced():
+        if create_autora_hub_requirements(source_branch, requirements_file):
+            create_autora_example_project()
+    else:
+        setup_basic(requirements_file)
     clean_up()
 
 
